4f9ca9ac0878d9a9bb1a1ab3cf10772dc8dc92fa
Will Johansson <will.johansson@rd.io>
2015-04-25 12:54:52 -0700

    Secrets

---
+++ SwiftlyYelp/secrets.swift
@@ -0,0 +1,12 @@
+//
+//  secrets.swift
+//  SwiftlyYelp
+//
+//  Created by Will Johansson on 2015-04-25.
+//  Copyright (c) 2015 Will Johansson. All rights reserved.
+//
+
+let yelpConsumerKey     = "YOUR_CONSUMER_KEY"
+let yelpConsumerSecret  = "YOUR_CONSUMER_SECRET"
+let yelpToken           = "YOUR_TOKEN"
+let yelpTokenSecret     = "YOUR_TOKEN_SECRET"
